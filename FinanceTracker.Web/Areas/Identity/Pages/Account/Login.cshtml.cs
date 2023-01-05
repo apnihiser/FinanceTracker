b989a0192e1c783268b4c50cdf8ecd53f532d6b4
Adam <apnihiser@gmail.com>
2023-01-05 15:25:48 -0500

    small bug where code was unreachable in login.cshtml.cs

--- FinanceTracker.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ FinanceTracker.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -87,9 +87,7 @@ namespace FinanceTracker.Web.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                {
-                    return RedirectToAction("Index", "Transaction");
-                }
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
