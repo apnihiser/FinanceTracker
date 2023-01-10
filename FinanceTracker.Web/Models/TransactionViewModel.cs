678181df82937108d67128be7e81d8db0451088a
Adam <apnihiser@gmail.com>
2023-01-10 17:57:30 -0500

    updated verbiage used in the what is a transaction accordian on the transaction index page

--- FinanceTracker.Web/Models/TransactionViewModel.cs
+++ FinanceTracker.Web/Models/TransactionViewModel.cs
@@ -7,20 +7,22 @@ namespace FinanceTracker.Web.Models
         public int Id { get; set; }
 
         [Required]
-        [Display(Name = "Account Name")]
+        [Display(Name = "Account")]
         public int AccountId { get; set; }
 
         [Required]
-        [Display(Name = "Provider Name")]
+        [Display(Name = "Category")]
         public int PayeeId { get; set; }
 
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Only numbers greater than zero allowed.")]
         [DataType(DataType.Currency)]
+        [Display(Name = "Dollar Amount")]
         public decimal AmountDue { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name = "Due Date")]
         public DateTime DueDate { get; set; }
 
         [Required]
@@ -28,17 +30,19 @@ namespace FinanceTracker.Web.Models
         public string? Status { get; set; }
 
         [StringLength(50, ErrorMessage = "{0} must be under 50 characters long.")]
+        [Display(Name = "Category")]
         public string? ProviderName { get; set; }
 
         [StringLength(100, ErrorMessage = "{0} must be under 50 characters long.")]
         public string? Service { get; set; }
 
         [StringLength(50, ErrorMessage = "{0} must be under 50 characters long.")]
+        [Display(Name = "Account")]
         public string? AccountName { get; set; }
 
         [StringLength(50, ErrorMessage = "{0} must be under 50 characters long.")]
         [Required]
-        [Display(Name = "Transaction Reason")]
+        [Display(Name = "Transfer Description")]
         public string Reason { get; set; }
 
         [StringLength(50, ErrorMessage = "{0} must be under 50 characters long.")]
